b52440b5c3369194688443f84c42eb205303c838
Stephen Samuels <stevesam1927@gmail.com>
2022-01-03 09:17:35 +0530

    Added Hands On Demos - Day 21

---
+++ 5. Angular 11/Day 22/Supporting Materials/sping-initializer-project/bike/src/test/java/com/globomantics/bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globomantics.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
